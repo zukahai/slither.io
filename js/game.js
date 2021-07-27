954fb7e6cd1bdfc40216d6b99da325eb8627d752
Phan Đức Hải <85035951+zukahai@users.noreply.github.com>
2021-07-27 11:21:25 +0700

    Add file

---
+++ js/game.js
@@ -0,0 +1,78 @@
+game_W = 0, game_H = 0;
+
+var bg_im = new Image();
+bg_im.src = "images/Map.png";
+
+class game {
+    constructor() {
+        this.canvas = null;
+        this.context = null;
+        this.init();
+    }
+
+    init() {
+        this.canvas = document.createElement("canvas");
+        this.context = this.canvas.getContext("2d");
+        document.body.appendChild(this.canvas);
+
+        this.render();
+        this.loop();
+
+        this.listenMouse();
+    }
+
+
+    listenMouse() {
+        document.addEventListener("mousedown", evt => {
+            var x = evt.offsetX == undefined ? evt.layerX : evt.offsetX;
+            var y = evt.offsetY == undefined ? evt.layerY : evt.offsetY;
+        })
+
+        document.addEventListener("mousemove", evt => {
+            var x = evt.offsetX == undefined ? evt.layerX : evt.offsetX;
+            var y = evt.offsetY == undefined ? evt.layerY : evt.offsetY;
+        })
+
+        document.addEventListener("mouseup", evt => {
+            var x = evt.offsetX == undefined ? evt.layerX : evt.offsetX;
+            var y = evt.offsetY == undefined ? evt.layerY : evt.offsetY;
+        })
+    }
+
+    loop() {
+        this.update();
+        this.draw();
+        setTimeout(() => this.loop(), 40);
+    }
+
+    update() {
+        this.render();
+    }
+
+ 
+    render() {
+        if (this.canvas.width != document.documentElement.clientWidth || this.canvas.height != document.documentElement.clientHeight) {
+            this.canvas.width = document.documentElement.clientWidth;
+            this.canvas.height = document.documentElement.clientHeight;
+            game_W = this.canvas.width;
+            game_H = this.canvas.height;
+        }
+    }
+
+    draw() {
+        this.clearScreen();
+    }
+
+    clearScreen() {
+        this.context.clearRect(0, 0, game_W, game_H);
+        this.context.fillStyle = '#000000';
+        this.context.fillRect(0 , 0, game_W, game_H);
+    }
+
+    getWidth() {
+        var area = game_W * game_H;
+        return Math.sqrt(area / 300);
+    }
+}
+
+var g = new game();
